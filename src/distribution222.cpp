0fa95986a53d245ebaf98bfe90a88e4c4215b879
Richard Taylor <rt@richardtaylor.co.uk>
2019-02-15 14:35:22 +0000

    Use random state not random scramble to estimate distribution

--- src/distribution222.cpp
+++ src/distribution222.cpp
@@ -3,33 +3,37 @@
 #include "Block2x2x2.h"
 #include "IterativeDeepening.h"
 #include "Logging.h"
-#include "RandomSequence.h"
-#include "Scrambler.h"
 #include "Sequence.h"
-#include "SequenceString.h"
 
 // Estimate the distribution of optimal solutions to a 2x2x2 block on
 // a 3x3x3 cube. Experience suggests that 5 moves is the most common.
+
+void report(int trials, int *frequency)
+{
+    LOG_REPORT << "Trials: " << trials;
+    LOG_REPORT << "Frequency distribution";
+
+    for (int n = 0; n <= 20; n++)
+    {
+        LOG_REPORT << n << " : " << frequency[n]
+                        << " : " << frequency[n] / double(trials);
+    }
+}
 
 int main()
 {
     Log::setLevel(Log::ERROR);
 
-    RandomSequence scramble(50);
+    Cube solved;
     int frequency[21] = { 0 };
-    int N = 10000;
+    int N = 100000;
 
     for (int i = 1; i <= N; i++)
     {
-        Sequence twists = scramble.next();
-        Cube cube;
-        Scrambler scrambler;
-
-        scrambler.scramble(cube, twists);
+        Cube cube = solved.randomise();
 
         IterativeDeepening solver;
         Block2x2x2 pattern;
-
         try
         {
             Sequence solution = solver.solve(cube, pattern);
@@ -42,14 +46,7 @@ int main()
         }
         if (i % 10 == 0)
         {
-            LOG_REPORT << "trials: " << i;
+            report(i, frequency);
         }
-    }
-
-    LOG_REPORT << "Frequency distribution";
-
-    for (int n = 0; n <= 20; n++)
-    {
-        LOG_REPORT << n << " : " << frequency[n] / double(N);
     }
 }
