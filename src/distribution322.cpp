89d9bd61170d3a6ca68bc5a1fbc656b755ee7217
Richard Taylor <rt@richardtaylor.co.uk>
2019-02-16 11:57:51 +0000

    Estimate the spread of 3x2x2 extensions

---
+++ src/distribution322.cpp
@@ -0,0 +1,54 @@
+
+#include <stdexcept>
+#include "Block3x2x2.h"
+#include "IterativeDeepening.h"
+#include "Logging.h"
+#include "Sequence.h"
+
+// Estimate the distribution of optimal solutions to extending a 2x2x2 block
+// to a 3x2x2 block on a 3x3x3 cube.
+//
+// Use a randomised cube that has a solved 2x2x2 block in the RUF corner.
+
+void report(int trials, int *frequency)
+{
+    LOG_REPORT << "Trials: " << trials;
+    LOG_REPORT << "Frequency distribution";
+
+    for (int n = 0; n <= 20; n++)
+    {
+        LOG_REPORT << n << " : " << frequency[n]
+                        << " : " << frequency[n] / double(trials);
+    }
+}
+
+int main()
+{
+    Log::setLevel(Log::ERROR);
+
+    Cube solved;
+    int frequency[21] = { 0 };
+    int N = 100000;
+
+    for (int i = 1; i <= N; i++)
+    {
+        Cube cube = solved.randomise( { RUF }, { RU, UF, RF } );
+
+        IterativeDeepening solver;
+        Block3x2x2 pattern(RUF);
+        try
+        {
+            Sequence solution = solver.solve(cube, pattern);
+
+            frequency[solution.length()]++;
+        }
+        catch (const std::runtime_error &e)
+        {
+            LOG_ERROR << "No solution found.";
+        }
+        if (i % 10 == 0)
+        {
+            report(i, frequency);
+        }
+    }
+}
